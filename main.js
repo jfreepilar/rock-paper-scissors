9ebbf9ef0c536c04fd9d25724f86a7123752fc14
jreepilar <tipsjsp@gmail.com>
2024-01-30 00:51:05 +0800

    FINISH code until compare the user and computer selection

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 //rock paper scissors
 
+//algorithm
+//1. ACCESS the values of the buttons and convert to array if possible
+//2. CREATE a function that will return the value of the selected button then include in an event handler
+//3. CALL the play round function everytime a button is clicked
+
  //generate random input
  function computerChoice() { 
     const hand = ["rock", "paper", "scissors"];
     const randomArr = Math.floor(Math.random()*hand.length);
     result = hand[randomArr];
-    return result;
+    return result; 
     }
 
-//prompt user to input rock paper scissors
-function playerChoice() {
-    let string = prompt("Please choose between rock,paper, and scissors: ");
-    let player = string.toLocaleLowerCase();
-    return player;
-}
-
-let userSelection = playerChoice();
 let computerSelection = computerChoice();
+let userSelection;
+let roundResult;
 
 //compare the input from the user and computer, single round
 function playRound(userSelection, computerSelection) {
@@ -37,48 +36,17 @@ function playRound(userSelection, computerSelection) {
     }
 }
 
-let roundResult = playRound(userSelection, computerSelection);
-
-//record the score 
-function scoreTab() {
-    let user = 0,
-    computer = 0;
-    let roundResult = playRound(userSelection, computerSelection);
-        if  ( roundResult == "user") {
-            user++;
-            }   else if (roundResult == "computer") {
-                computer++;
-                }
-            } 
-console.log(playRound(userSelection, computerSelection));
-scoreTab();
-console.log(scoreTab());
-
-//reiterate the game in a best-of-5 series
-function game() {
-    let user = 0,
-    computer = 0;
-
-    do {
-        let userSelection = playerChoice();
-        let computerSelection = computerChoice();
-        let roundResult = playRound(userSelection, computerSelection);
-
-        if (roundResult === "user") {
-        user++;
-        } else if (roundResult === "computer") {
-        computer++;
-        }
-
-        console.log("User Score:", user);
-        console.log("Computer Score:", computer);
-    } while (user < 3 && computer < 3);
-
-    console.log("Final Scores:");
-    console.log("User:", user);
-    console.log("Computer:", computer);
-}
-
-// Call the game function to start the game
-game();
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach( function(button) {
+    //function 1 RETURN the value of the clicked button
+    button.addEventListener('click', () => {
+        userSelection = button.value;
+        console.log(userSelection);
+    
+    //function 2 COMPARE the user's and computer's selection
+    playRound(userSelection, computerSelection);
+        roundResult = playRound(userSelection, computerSelection);
+        console.log(roundResult);
+    })
+});
 
